a992f06ea5947094ce60bcbaf04271539b7a1519
DESKTOP-O20T0TA\mr4-07 <qkfmf@naver.com>
2024-01-12 15:25:49 +0900

    1.보드 insert,select 기능 구현

---
+++ com.mire.cinema/src/main/java/com/mire/cinema/repository/NoticeMapper.java
@@ -0,0 +1,33 @@
+package com.mire.cinema.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.stereotype.Repository;
+
+import com.mire.cinema.domain.notice.Notice;
+import com.mire.cinema.domain.notice.NoticeDTO;
+
+@Repository
+@Mapper
+@MapperScan
+public interface NoticeMapper {
+	// 공지사항 등록하기
+	void insertNotice(Notice notice);
+
+	// 공지사항 리스트 출력하기
+	List<Notice> selectNotice();
+	
+	// 공지사항 내용 출력하기
+	Notice getNotice(Long boardNo);
+
+	// 공지사항 내용 수정하기
+	void updateNotice(NoticeDTO.NoticeUpdate notice);
+	
+	// 공지사항 삭제하기
+	void deleteNotice(Long boardNo);
+
+	//공지사항 조회수 증가 
+	void updateNoticeViews(Notice notice);
+}
