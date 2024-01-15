3269739d9583f77e989185d8f9ee3e8c8268f167
bjkim247 <bjkim247@gmail.com>
2024-01-15 17:26:26 +0900

    cinema update 기능 수정중

--- com.mire.cinema/src/main/java/com/mire/cinema/repository/CinemaMapper.java
+++ com.mire.cinema/src/main/java/com/mire/cinema/repository/CinemaMapper.java
@@ -6,13 +6,14 @@ import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import com.mire.cinema.domain.cinema.Cinema;
+import com.mire.cinema.domain.cinema.CinemaDTO;
 
 @Repository
 @Mapper
 public interface CinemaMapper {
 
 
-	public void update(Cinema cinema) throws Exception;
+	public void updateCinema(Cinema cinema) throws Exception;
 
 	public List<Cinema> list() throws Exception;
 }
