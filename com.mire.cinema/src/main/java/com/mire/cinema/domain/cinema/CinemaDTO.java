9608aabf527aa1878608adef6188087cc092124c
bjkim247 <bjkim247@gmail.com>
2024-01-16 13:28:01 +0900

    영화관 리스트 조회,수정 완료

--- com.mire.cinema/src/main/java/com/mire/cinema/domain/cinema/CinemaDTO.java
+++ com.mire.cinema/src/main/java/com/mire/cinema/domain/cinema/CinemaDTO.java
@@ -1,5 +1,7 @@
 package com.mire.cinema.domain.cinema;
 
+import java.time.LocalDate;
+
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,20 +12,39 @@ public class CinemaDTO {
 	@Data
 	@AllArgsConstructor
 	@NoArgsConstructor
+	public static class CinemaResponseDTO {
+		private boolean result;
+		private String message;
+	}
+
+	@Data
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class CinemaWriteDTO {
+		private Long cinemaNo;
+		@NotBlank(message = "영화관 이름을 입력하세요")
+		private String cinemaName;
+		@NotBlank(message = "영화관 소개를 입력하세요")
+		private String cinemaIntro;
+		private String cinemaTotalScreen;
+		private String cinemaPhone;
+		private String cinemaLocation;
+	}
+
+	@Data
+	@AllArgsConstructor
+	@NoArgsConstructor
 	public static class CinemaUpdate {
 		private Long cinemaNo;
 		@NotBlank(message = "영화관 이름을 입력하세요")
 		private String cinemaName;
 		@NotBlank(message = "영화관 소개를 입력하세요")
 		private String cinemaIntro;
-		@NotBlank	(message = "영화관 총상영관 수를 입력하세요")
+		@NotBlank(message = "영화관 총 상영관수를 입력하세요")
 		private String cinemaTotalScreen;
-		@NotBlank(message = "영화관 전화번호를 입력하세요")
+		@NotBlank(message = "영화관 전화 번호를 입력하세요")
 		private String cinemaPhone;
 		@NotBlank(message = "영화관 위치를 입력하세요")
 		private String cinemaLocation;
 	}
-	
-	
-
 }
