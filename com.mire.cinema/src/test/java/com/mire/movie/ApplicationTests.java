9873a87bac2af5cc70d7255988f4856c1e29aba3
DESKTOP-O20T0TA\mr4-07 <qkfmf@naver.com>
2024-01-17 17:48:16 +0900

    test 중

--- com.mire.cinema/src/test/java/com/mire/movie/ApplicationTests.java
+++ com.mire.cinema/src/test/java/com/mire/movie/ApplicationTests.java
@@ -8,9 +8,12 @@ import org.springframework.test.context.ContextConfiguration;
 import com.mire.cinema.Application;
 import com.mire.cinema.domain.member.MemberDTO;
 import com.mire.cinema.domain.member.MemberDTO.search;
+import com.mire.cinema.domain.notice.NoticeDTO;
 import com.mire.cinema.repository.MemberMapper;
+import com.mire.cinema.repository.NoticeMapper;
 import com.mire.cinema.service.MemberService;
 import com.mire.cinema.service.MovieService;
+import com.mire.cinema.service.NoticeService;
 
 import lombok.extern.java.Log;
 
@@ -19,22 +22,17 @@ import lombok.extern.java.Log;
 @ContextConfiguration(classes = Application.class)
 class ApplicationTests {
 	@Autowired
-	MemberService memberService;
-	
+	NoticeService movieService;
 	@Autowired
-	MovieService movieService;
-	@Autowired
-	MemberMapper mapper;
+	NoticeMapper mapper;
 
-	 @Test
-	    void testSelectItemGiftCard() {
-	       
-		 	MemberDTO.search  dto= new search();
-		 	dto.setStartNum(1);
-		 	dto.setEndNum(2);
-		 	dto.setMemberId("admin");
-	       mapper.countsearchMember("hi");
-	       mapper.searchPartList(dto).forEach(data -> log.info(data.toString()));
-	        
-	    }
+	@Test
+	void testSelectItemGiftCard() {
+		NoticeDTO.Search dto = new NoticeDTO.Search();
+		dto.setStartNum(1);
+		dto.setEndNum(2);
+		dto.setBoardTitle("미래");
+		mapper.searchNoticeList(dto).forEach(data -> log.info(data.toString()));
+
+	}
 }
