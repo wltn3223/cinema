f5b01ad5bfef20d374af11bb00f7fba90b82bc4a
DESKTOP-O20T0TA\mr4-07 <qkfmf@naver.com>
2024-01-23 09:33:30 +0900

    충돌수정

--- com.mire.cinema/src/test/java/com/mire/movie/ApplicationTests.java
+++ com.mire.cinema/src/test/java/com/mire/movie/ApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 import com.mire.cinema.Application;
-<<<<<<< HEAD
 import com.mire.cinema.domain.itemgiftcard.ItemGiftCardDTO;
 import com.mire.cinema.domain.member.MemberDTO;
 import com.mire.cinema.domain.member.MemberDTO.search;
@@ -20,11 +19,9 @@ import com.mire.cinema.service.ItemGiftCardServiceImpl;
 import com.mire.cinema.service.MemberService;
 import com.mire.cinema.service.MovieService;
 import com.mire.cinema.service.MovieServiceImpl;
-=======
 import com.mire.cinema.domain.reserve.ReserveDTO;
 import com.mire.cinema.domain.reserve.ReserveDTO.Count;
 import com.mire.cinema.repository.ReserveMapper;
->>>>>>> 7fd80804ca3e58deb7ecb00897d4c92b1d4087fe
 
 import lombok.extern.java.Log;
 
@@ -32,7 +29,6 @@ import lombok.extern.java.Log;
 @SpringBootTest
 @ContextConfiguration(classes = Application.class)
 class ApplicationTests {
-<<<<<<< HEAD
 	@Autowired
 	MovieService Service;
 	
@@ -41,28 +37,18 @@ class ApplicationTests {
 	
 	@Autowired
     MovieMapper mapper;
-=======
->>>>>>> 7fd80804ca3e58deb7ecb00897d4c92b1d4087fe
 
 	@Autowired
 	ReserveMapper mapper2;
 	 @Test
 	    void testSelectItemGiftCard() {
-<<<<<<< HEAD
 	       System.out.println(Service.findSearchMovie("무서운"));
-=======
 	    
-	       
-	    
-	      
-	       
 	       ReserveDTO.Count count = new Count();
 	       count.setStart(1);
 	       count.setEnd(3);
 	       count.setMemberId("wltn3223");
 	       System.out.println(mapper2.getPartMyList(count).size());
-	
 	        
->>>>>>> 7fd80804ca3e58deb7ecb00897d4c92b1d4087fe
 	    }
 }
